a8cad7b9c6685d7258a3ea69363dbfe905b62d59
Gevorg Arzumanjan <gevorgarzumanjan@Gevorgs-MacBook-Air.local>
2022-02-10 20:56:15 +0100

    Geolocation: created basic setup for geolocation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
 import styles from './App.module.scss';
 
-function App() {
+const App = () => {
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+  const [geolocationStatus, setGeolocationStatus] = useState();
+
+  useEffect(() => {
+    navigator.geolocation
+      ? navigator.geolocation.getCurrentPosition(position => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+          setGeolocationStatus('Success!');
+        }, error => setGeolocationStatus(error.message))
+      : setGeolocationStatus('Geolocation is not supported by this browser!');
+  }, []);
+
   return (
     <div className={styles.app}>
       <p>Weather Application</p>
+      <p>Latitude: {latitude}</p>
+      <p>Longitude: {longitude}</p>
+      <p>geolocationStatus: {geolocationStatus}</p>
     </div>
   );
 }
