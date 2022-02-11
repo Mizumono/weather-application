733bd825c4ddf775d1b1c8158d497babc6b7f690
Gevorg Arzumanjan <gevorgarzumanjan@Gevorgs-MacBook-Air.local>
2022-02-11 09:45:34 +0100

    Openweathermap: created basic setup for openweathermap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import styles from './App.module.scss';
+import axios from 'axios';
 
 const App = () => {
   const [latitude, setLatitude] = useState();
   const [longitude, setLongitude] = useState();
   const [geolocationStatus, setGeolocationStatus] = useState();
+
+  const [currentWeather, setCurrentWeather] = useState();
 
   useEffect(() => {
     navigator.geolocation
@@ -16,12 +19,24 @@ const App = () => {
       : setGeolocationStatus('Geolocation is not supported by this browser!');
   }, []);
 
+  useEffect(() => {
+    latitude && longitude && axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={API key}&units=metric`)
+    .then(({data}) => {
+      setCurrentWeather(data);
+    })
+    .catch(error => {
+      console.warn(error);
+    });
+  }, [latitude, longitude])
+
   return (
     <div className={styles.app}>
       <p>Weather Application</p>
       <p>Latitude: {latitude}</p>
       <p>Longitude: {longitude}</p>
       <p>geolocationStatus: {geolocationStatus}</p>
+      <p>Name: {currentWeather && currentWeather.name}</p>
+      <p>Temp: {currentWeather && currentWeather.main.temp}</p>
     </div>
   );
 }
