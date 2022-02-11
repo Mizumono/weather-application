a2fd1f828b8ec5832dbfcedceeb1afc85e145293
Gevorg Arzumanjan <gevorgarzumanjan@Gevorgs-MacBook-Air.local>
2022-02-11 23:07:11 +0100

    Card: created card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import styles from './Card.module.scss';
+
+const Card = ({ children }) => {
+  return (
+    <div className={styles.card}>
+      {children}
+    </div>
+  )
+};
+
+Card.Header = ({ children }) => <div className={styles.header}>{children}</div>;
+Card.Body = ({ children }) => <div className={styles.body}>{children}</div>;
+Card.Footer = ({ children }) => <div className={styles.footer}>{children}</div>;
+
+export default Card;
