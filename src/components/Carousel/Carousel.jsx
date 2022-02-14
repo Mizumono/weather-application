3b4ae92b343b9a52cc73528a29f145bbe8ea9983
Gevorg Arzumanjan <gevorgarzumanjan@Gevorgs-MacBook-Air.local>
2022-02-14 10:48:05 +0100

    Carousel: created basic carousel component

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import style from './Carousel.module.scss';
+
+const Carousel = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateIndex(activeIndex + 1);
+    }, 2000);
+
+    return () => interval && clearInterval(interval);
+  });
+
+  const updateIndex = newIndex => {
+    newIndex >= React.Children.count(children) ? setActiveIndex(0) : setActiveIndex(newIndex);
+  };
+
+  return (
+    <div className={style.carousel}>
+      <div className={style.inner}>
+        {React.Children.map(children, (child, index) => {
+          return React.cloneElement(child, { activeIndex: activeIndex === index });
+        })}
+      </div>
+    </div>
+  );
+};
+
+Carousel.Item = ({ children, activeIndex }) => <div className={`${style.carouselItem} ${activeIndex && style.active}`}>{children}</div>;
+
+export default Carousel;
