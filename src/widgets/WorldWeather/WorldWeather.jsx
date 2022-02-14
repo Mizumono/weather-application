a9a55406405598dd228c37d7b58c1ea8b95a26e9
Gevorg Arzumanjan <gevorgarzumanjan@Gevorgs-MacBook-Air.local>
2022-02-14 12:46:27 +0100

    WorldWeather: populated the widget with the API call response and updated the Card component

--- src/widgets/WorldWeather/WorldWeather.jsx
+++ src/widgets/WorldWeather/WorldWeather.jsx
@@ -1,20 +1,49 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from './WorldWeather.module.scss';
 import Carousel from '../../components/Carousel/Carousel';
 import PropTypes from 'prop-types';
 import Card from '../../components/Card/Card';
 
-const WorldWeather = ({ latitude, longitude }) => {
+const WorldWeather = ({ cities }) => {
+  const requestArray = cities.map(city => `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid={API key}&units=metric`)
+  const [worldWeather, setWorldWeather] = useState();
+  const [worldWeatherStatus, setWorldWeatherStatus] = useState();
+
+  useEffect(() => {
+    cities && axios.all(requestArray.map(request => axios.get(request)))
+    .then((response) => {
+      setWorldWeather(response);
+      setWorldWeatherStatus('Success!')
+    })
+    .catch(error => {
+      setWorldWeatherStatus(error);
+    });
+  }, [cities])
+
   return (
-    <Carousel>
-      <Carousel.Item><Card>1</Card></Carousel.Item>
-      <Carousel.Item><Card>2</Card></Carousel.Item>
-      <Carousel.Item><Card>3</Card></Carousel.Item>
-    </Carousel>
+    worldWeather ? (
+      <Carousel>
+        {worldWeather.map((item, index) => {
+          return (
+            <Carousel.Item key={index}>
+              <Card>
+                <Card.Body>
+                  <div className={styles.content}>
+                    <h2 className={styles.city}>{item.data.name}</h2>
+                    <p className={styles.currentTemperature}><span>{Math.round(item.data.main.temp_max)}&#176;</span> / <span className={styles.tempMin}>{Math.round(item.data.main.temp_min)}&#176;</span></p>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Carousel.Item>)
+        })}
+      </Carousel>
+    ) : <p>Loading...</p>
   )
-}
+};
 
 WorldWeather.propTypes = {
-  latitude: PropTypes.number,
-  longitude: PropTypes.number,
+  cities: PropTypes.arrayOf(PropTypes.number),
 };
 
 export default WorldWeather;
